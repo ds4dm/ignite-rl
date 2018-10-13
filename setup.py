a0fd98fb15b10efbc4403c2c19b0c7a7949df594
AntoinePrv <antoine.prouvost.2013_dev@polytechnique.org>
2018-10-13 12:04:26 -0400

    Packaging setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Ignite-Rl",
+    version="0.1",
+    packages=find_packages(),
+    author="Antoine Prouvost",
+    license="MIT",
+    url="github.com/ds4dm/ignite-rl",
+    description="Collection of ignite engines for reinforcment learning.",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "numpy",
+        "torch",
+        "pytorch-ignite"
+    ]
+)
