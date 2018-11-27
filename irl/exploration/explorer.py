466516e0f336545c9997cad540165147f71eabc4
AntoinePrv <antoine.prouvost.2013_dev@polytechnique.org>
2018-11-27 18:50:09 -0500

    Remove apply to tensor in explorer

--- irl/exploration/explorer.py
+++ irl/exploration/explorer.py
@@ -21,7 +21,6 @@ import attr
 import torch
 from ignite.engine import Engine, Events
 
-import irl.utils as utils
 from .environment import Observation, Action, Environment
 
 
@@ -90,8 +89,7 @@ def create_explorer(
 
         # Make action.
         next_observation, reward, done, infos = env.step(action)
-        next_observation = utils.apply_to_tensor(
-            next_observation, lambda t: t.to(dtype=dtype))
+        next_observation = next_observation.to(dtype=dtype)
 
         # We create the transition object and store it.
         engine.state.transition = __Transition(
@@ -109,8 +107,7 @@ def create_explorer(
 
         # Save for next move
         if device is not None and torch.device(device).type == "cuda":
-            engine.state.observation = utils.apply_to_tensor(
-                next_observation, lambda t: t.pin_memory())
+            engine.state.observation = next_observation.pin_memory()
         else:
             engine.state.observation = next_observation
 
@@ -121,14 +118,12 @@ def create_explorer(
 
     @explorer.on(Events.ITERATION_STARTED)
     def _move_to_device(engine):
-        engine.state.observation = utils.apply_to_tensor(
-            engine.state.observation,
-            lambda t: t.to(device=device, non_blocking=True))
+        engine.state.observation = engine.state.observation.to(
+            device=device, non_blocking=True)
 
     @explorer.on(Events.EPOCH_STARTED)
     def _init_episode(engine):
-        engine.state.observation = utils.apply_to_tensor(
-            env.reset(), lambda t: t.to(dtype=dtype))
+        engine.state.observation = env.reset().to(dtype=dtype)
 
     @explorer.on(Events.COMPLETED)
     def _close(engine):
